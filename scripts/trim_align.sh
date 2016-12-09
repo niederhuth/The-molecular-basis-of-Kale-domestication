dab90086eef40a5c04438b72399ddda45489f260
chadn737 <chadn737@gmail.com>
2016-12-08 19:40:01 -0500

    Added folders and other things

---
+++ scripts/trim_align.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "Starting"
+mkdir raw_QC trimmed_QC
+
+#Unpack fastqs
+cd fastq
+bunzip2 *.fastq.bz2
+cd ..
+
+#QC analysis raw data
+echo "First QC analysis"
+export PATH=${PATH}:/usr/local/fastqc/latest/
+time /usr/local/fastqc/latest/fastqc --noextract -t 10 -a ../../misc/TruSeq.tsv -o raw_QC fastq/*fastq 
+
+#Trim data
+echo "Trimming data"
+time python2.7 /usr/local/cutadapt/1.9.dev1/bin/cutadapt --trim-n --max-n 0.1 -m 30 -q 20 -e 0.1 -O 3 -a file:../../misc/TruSeq.fa  -o fastq/trimmed.fastq fastq/*fastq
+
+#QC analysis trimmed data
+echo "Second QC analysis"
+time /usr/local/fastqc/latest/fastqc --noextract -t 10 -a ../../misc/TruSeq.tsv -o trimmed_QC fastq/trimmed.fastq 
+
+#Align data
+echo "Aligning data"
+export PATH=/usr/local/samtools/0.1.18/:${PATH}
+export LD_LIBRARY_PATH=/usr/local/boost/1.54.0/gcc447/lib:/usr/local/gcc/4.7.1/lib:/usr/local/gcc/4.7.1/lib64:${LD_LIBRARY_PATH}
+time /usr/local/tophat/2.0.14/bin/tophat -F 0.1 --transcriptome-index ../ref/transcriptome/Gmax_v2 --no-coverage-search --b2-sensitive --no-mixed --read-realign-edit-dist 0 -i 30 -M -I 10000 -p 10 --library-type fr-firststrand -o tophat ../ref/bowtie2/Gmax_v2 fastq/trimmed.fastq
+
+#Repackage everything
+echo "Wrapping up"
+cd fastq
+for i in *fastq
+do
+bzip2 "$i"
+done
+cd ..
+
+echo "Done"
