1ce2b3206c37c9a244c4cbe808fc7ec355563288
Chad <chadn737@gmail.com>
2016-12-08 22:14:38 -0500

    Working

--- scripts/prepare_references.sh
+++ scripts/prepare_references.sh
@@ -21,10 +21,9 @@ echo "Making transcriptome index"
 cd transcriptome
 export PATH=/usr/local/samtools/0.1.18/:${PATH}
 export LD_LIBRARY_PATH=/usr/local/boost/1.54.0/gcc447/lib:/usr/local/gcc/4.7.1/lib:/usr/local/gcc/4.7.1/lib64:${LD_LIBRARY_PATH}
-time /usr/local/tophat/2.0.14/bin/tophat -G ../misc/Boleracea_v2.1.31_fixed.gtf --transcriptome-index Boleracea_v2.1.31 ../bowtie2/Boleracea_v2.1.31
-mv Boleracea_v2.1.31/* ../transcriptome/
-rmdir Boleracea_v2.1.31/
-rm -R tophat_out/
+time /usr/local/tophat/2.0.14/bin/tophat -G ../misc/Boleracea_v2.1.31_fixed.gtf --transcriptome-index tmp/Boleracea_v2.1.31 ../bowtie2/Boleracea_v2.1.31
+mv tmp/* ../transcriptome/
+rm -R tophat_out/ tmp/
 cd ../
 
 echo "Done"
