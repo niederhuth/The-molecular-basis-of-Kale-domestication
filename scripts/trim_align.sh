8834e27a254bd0d1d95ad3ea177ff950f38b083f
chadn737 <chadn737@gmail.com>
2016-12-12 16:34:09 -0500

    Updated trim_align.sh

--- scripts/trim_align.sh
+++ scripts/trim_align.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Starting"
-mkdir raw_QC2 trimmed_QC2
+mkdir raw_QC trimmed_QC
 
 #Unpack fastqs
 cd fastq
@@ -11,29 +11,37 @@ cd ..
 #QC analysis raw data
 echo "First QC analysis"
 export PATH=${PATH}:/usr/local/fastqc/latest/
-time /usr/local/fastqc/latest/fastqc --noextract -t 10 -a ../../misc/evrogen_mint.tsv -o raw_QC2 fastq/*fastq 
+time /usr/local/fastqc/latest/fastqc --noextract -t 4 -a ../../misc/evrogen_mint.tsv -o raw_QC fastq/*fastq 
 
 #Trim data
 echo "Trimming data"
-time python2.7 /usr/local/cutadapt/1.9.dev1/bin/cutadapt --trim-n --max-n 0.1 -m 30 -q 20 -e 0.1 -O 3 -a file:../../misc/evogren_mint.fa  -o trimmed2.fastq fastq/*fastq
+time python2.7 /usr/local/cutadapt/1.9.dev1/bin/cutadapt --quality-base=64 --trim-n --max-n 0.1 -m 30 -q 20,20 -e 0.1 -O 3 -a file:../../misc/evrogen_mint.fa  -o fastq/tmp.fastq fastq/*fastq
+time /usr/local/fastx_toolkit/0.0.14/bin/fastx_trimmer -i fastq/tmp.fastq -o fastq/tmp2.fastq -m 30 -f 2
+perl /usr/local/prinseq/0.20.3/prinseq-lite.pl -verbose -fastq tmp2.fastq -min_len 30 -out_format 3 -out_good trimmed.fastq -out_bad tmp3.fastq
+rm fastq/tmp.fastq fastq/tmp2.fastq fastq/tmp3.fastq
 
 #QC analysis trimmed data
 echo "Second QC analysis"
-time /usr/local/fastqc/latest/fastqc --noextract -t 10 -a ../../misc/TruSeq.tsv -o trimmed_QC2 trimmed2.fastq 
+time /usr/local/fastqc/latest/fastqc --noextract -t 4 -a ../../misc/evrogen_mint.tsv -o trimmed_QC fastq/trimmed.fastq 
 
 #Align data
 echo "Aligning data"
 export PATH=/usr/local/samtools/0.1.18/:${PATH}
 export LD_LIBRARY_PATH=/usr/local/boost/1.54.0/gcc447/lib:/usr/local/gcc/4.7.1/lib:/usr/local/gcc/4.7.1/lib64:${LD_LIBRARY_PATH}
-time /usr/local/tophat/2.0.14/bin/tophat -F 0.1 --transcriptome-index ../ref/transcriptome/Gmax_v2 --no-coverage-search --b2-sensitive --no-mixed --read-realign-edit-dist 0 -i 30 -M -I 10000 -p 10 --library-type fr-firststrand -o tophat2 ../ref/bowtie2/Gmax_v2 trimmed2.fastq
+time /usr/local/tophat/2.0.14/bin/tophat -F 0.1 --phred64-quals --transcriptome-index ../ref/transcriptome/Boleracea_v2.1.31 --no-coverage-search --b2-sensitive --no-mixed --read-realign-edit-dist 0 -i 30 -M -I 10000 -p 4 --library-type fr-unstranded -o tophat ../ref/bowtie2/Boleracea_v2.1.31 fastq/trimmed.fastq
+
+#Count reads per feature
+echo "Running htseq-count"
+export PYTHONPATH=${PYTHONPATH}:/usr/local/htseq/0.6.1p1/lib/python
+time /usr/local/htseq/0.6.1p1/bin/htseq-count -f bam -s no -t exon -i gene_id -m union tophat/accepted_hits.bam ../ref/transcriptome/Boleracea_v2.1.31.gff > gene_counts.tsv
 
 #Repackage everything
-#echo "Wrapping up"
-#cd fastq
-#for i in *fastq
-#do
-#bzip2 "$i"
-#done
-#cd ..
+echo "Wrapping up"
+cd fastq
+for i in *fastq
+do
+bzip2 "$i"
+done
+cd ..
 
 echo "Done"
---
+++ status.md
@@ -0,0 +1,13 @@
+---
+Daily status of the analysis.
+---
+
+#### Dec. 12, 2016
+1. Data is phred64 
+2. Optimized preprocessing and alignment of data.
+ * 5-prime and 3-prime trimming increased mapping over 3-trimming alone.
+ * Trimming to quality score of 20 performed better than trimming to quality score of 13.
+ * High occurance (>90%) of "A" at first base indicated some sort of contamination in read even after trimming. Removing first base from all reads greatly improved mapping further. 
+ * Keeping reads <30bp (min length 25bp) gives a weird GC content skew. Filter out reads <30bp.
+3. Run trim_align.sh on all data.
+4. 
